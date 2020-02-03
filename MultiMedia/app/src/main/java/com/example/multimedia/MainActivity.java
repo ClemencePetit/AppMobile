084b0cc3b605ea095e10e2b3241328fb61619d8d
Unknown <clemence.ptii@gmail.com>
2020-02-02 22:22:13 -0500

    ça plante. LOL

--- MultiMedia/app/src/main/java/com/example/multimedia/MainActivity.java
+++ MultiMedia/app/src/main/java/com/example/multimedia/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.multimedia;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Message;
@@ -19,7 +21,6 @@ import android.os.RemoteException;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,38 +29,38 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListOfSongs listOfSongs;
+    public ListOfSongs listOfSongs;
     private Messenger mService = null;
     boolean bound;
+    boolean sent=false;
+    //boolean isPlaying=false;
+    IntentFilter filter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //on récupère la liste des musiques
         setContentView(R.layout.activity_main);
+
+
 
         final String musicOnly = MediaStore.Audio.Media.IS_MUSIC + " != 0 ";
 
         //AJOUTER PROTECTION
 
         listOfSongs = new ListOfSongs(this,musicOnly);
-
-        //On affiche la liste des musiques
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.simplerow, listOfSongs.getNames());
         ListView listView =(ListView) findViewById(R.id.listMusics);
         listView.setAdapter(adapter);
 
+        filter = new IntentFilter();
+        filter.addAction("isPlaying");
 
-        //Linking with service
-        Intent intent = new Intent(this, MessengerService.class);
-        ArrayList<String> paths = listOfSongs.getPaths();
-        intent.putExtra("allPaths", paths);
+        registerReceiver(mMBroadcastReceiver, filter);
 
-        startService(intent);
+
     }
 
-    private ServiceConnection mConnectier = new ServiceConnection() {
+    private ServiceConnection mConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             mService=new Messenger(service);
@@ -73,33 +74,93 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    public void sayHello(View v){
-        if(!bound) return;
-        Message msg = Message.obtain(null, MessengerService.MSG_SAY_HELLO,0,0);
-        try{
-            mService.send(msg);
-        } catch (RemoteException e){
-            e.printStackTrace();
-        }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        bindService(new Intent(this, MessengerService.class),mConnection, Context.BIND_AUTO_CREATE);
+
     }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(bound)
+        {
+            unbindService(mConnection);
+            bound=false;
+        }
+        unregisterReceiver(mMBroadcastReceiver);
+    }
+
+
 
     public void playThis(View v){
         TextView Tv = (TextView) v;
-        Toast.makeText(getApplicationContext(),"Playing : " + Tv.getText(),Toast.LENGTH_SHORT).show();
         int index = listOfSongs.getNames().indexOf(Tv.getText());
-        //on change l'index de la chanson joué par le mediaplayer
-        MessengerService.changeIndex(index);
+        sendMessagePlay(index);
     }
 
-    public void play(View v){
-        //on démarre le media player
-        MessengerService.start();
+    public void play(View v) {
+
+       if(!sent) {
+            sendMessagePlay(0);
+        }
+        else {
+            Message message;
+            /*if (isPlaying) {
+                message= Message.obtain(null, MessengerService.MSG_PAUSE,0,0);
+                //isPlaying = false;
+            } else {
+                message= Message.obtain(null, MessengerService.MSG_UNPAUSE,0,0);
+                //isPlaying = true;
+            }*/
+            message= Message.obtain(null, MessengerService.MSG_PAUSE,0,0);
+            try{
+                mService.send(message);
+            } catch (RemoteException e){
+                e.printStackTrace();
+            }
+
+        }
+
     }
-    /*
-    @Override
-    protected void OnStart(){
-        super.onStart();
-        bindService(new Intent(this, MessengerService.class),mConnection,)
+
+    public void sendMessagePlay(int index)
+    {
+        Message message;
+        if(!bound) return;
+        if(!sent)
+        {
+            Bundle bundle = new Bundle();
+            bundle.putStringArrayList("paths",listOfSongs.getPaths());
+            message = Message.obtain(null, MessengerService.MSG_SEND_PATHS,index,0);
+            message.setData(bundle);
+            sent=true;
+        }
+        else
+        {
+            message= Message.obtain(null, MessengerService.MSG_PLAY,index,0);
+        }
+        try{
+            mService.send(message);
+        } catch (RemoteException e){
+            e.printStackTrace();
+        }
+        //isPlaying=true;
     }
-*/
+
+   private BroadcastReceiver mMBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Get extra data included in the Intent
+            Toast.makeText(context,""+intent.getBooleanExtra("Status",false),Toast.LENGTH_SHORT).show();
+            //isPlaying = intent.getBooleanExtra("Status",false);
+            //Toast.makeText(getApplicationContext(),"playing : "+message,Toast.LENGTH_SHORT).show();
+            // Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
 }
+
+
--- MultiMedia/app/src/main/java/com/example/multimedia/MessengerService.java
+++ MultiMedia/app/src/main/java/com/example/multimedia/MessengerService.java
@@ -5,42 +5,92 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.os.Messenger;
-import android.provider.MediaStore;
-import android.widget.Toast;
 import android.os.Bundle;
+import android.os.Messenger;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
+import android.widget.Toast;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import java.util.ArrayList;
-import java.util.Locale;
-
-import static android.media.session.PlaybackState.ACTION_PLAY;
 
 
 public class MessengerService extends Service {
-    public static final Intent ACTION_PLAY = new Intent("com.example.multimedia.ACTION_PLAY");
-    static final int MSG_SAY_HELLO=1;
-
-    public static MediaPlayer mp;
+    static final int MSG_SEND_PATHS=2;
+    static final int MSG_PLAY=3;
+    static final int MSG_PAUSE=4;
+    //static final int MSG_UNPAUSE=5;
     private static ArrayList<String> musicPaths;
-    private static Context context;
-    private static int index;
 
-    final String musicOnly = MediaStore.Audio.Media.IS_MUSIC + " != 0 ";
+    private static Context context;
+
+    public static MediaPlayer mp=new MediaPlayer();
+    private static int currentIndex;
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        //mp=new MediaPlayer();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
+            public void onCompletion(MediaPlayer mediaPlayer){
+                currentIndex = (currentIndex+1) % musicPaths.size() ;
+                setMusic();
+            }
+        });
+        context=this;
+    }
+
+    private void setMusic(){
+        mp.reset();
+        try { mp.setDataSource(this,Uri.parse((musicPaths.get(currentIndex)))); } catch (Exception e) {}
+        try { mp.prepare(); } catch (Exception e) {}
+        mp.start();
+    }
+
+   private void setPause(){
+        if(mp.isPlaying()) {
+            mp.pause();
+        }
+        else{
+            mp.start();
+        }
+       //sendMessageToActivity();
+    }
+
+    /*private void setUnpause(){
+        mp.start();
+    }*/
 
     class IncomingHandler extends Handler{
         @Override
         public void handleMessage(Message msg){
             switch (msg.what)
             {
-                case MSG_SAY_HELLO:
-                    Toast.makeText(getApplicationContext(),"hello",Toast.LENGTH_SHORT).show();
+                case MSG_SEND_PATHS:
+                    musicPaths=msg.getData().getStringArrayList("paths");
+                    currentIndex=msg.arg1;
+                    setMusic();
                     break;
+                case MSG_PLAY:
+                    currentIndex=msg.arg1;
+                    setMusic();
+                    break;
+                case MSG_PAUSE:
+                    setPause();
+                    //sendMessageToActivity();
+                    break;
+                /*case MSG_UNPAUSE:
+                    setUnpause();
+                    //sendMessageToActivity();
+                    break;*/
                     default:
                         super.handleMessage(msg);
             }
+
+            //sendMessageToActivity();
         }
     }
 
@@ -52,46 +102,25 @@ public class MessengerService extends Service {
     }
 
     @Override
-    public void onCreate(){
-     mp  = new MediaPlayer();
+    public void onDestroy() {
+
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId){
-        //musicPaths = null;
-        context = this;
-        int index = 1;
-        //String path = "";
-        Bundle extras = intent.getExtras();
-        if(extras != null){
-            musicPaths = (ArrayList<String>) extras.get("allPaths");
-        }
-        return START_STICKY;
+    /*public boolean isPlaying()
+    {
+        return mp.isPlaying();
+    }*/
+
+    private void sendMessageToActivity() {
+        Intent intent = new Intent();//context,MainActivity.class);
+        // You can also include some extra data.
+        intent.putExtra("Status", mp.isPlaying());
+        intent.setAction("isPlaying");
+        //LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+        sendBroadcast(intent);
+
+        Toast.makeText(context,"pifpafpouf",Toast.LENGTH_SHORT).show();
+
     }
 
-    //play a song located in tha path given
-    private static void processPlayRequest(Uri path){
-        mp.reset();
-        mp = MediaPlayer.create(context, path);
-        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
-            public void onCompletion(MediaPlayer mediaPlayer){
-                index = (index+1) % musicPaths.size() ;
-                MessengerService.changeIndex(index);
-            }
-        });
-        mp.start();
-    }
-
-    //change the music according to newIndex
-    public static void changeIndex(int newIndex){
-        index = newIndex;
-        processPlayRequest(Uri.parse(musicPaths.get(newIndex)));
-    }
-
-    //start the mediaplayer from the first song
-    public static void start(){
-        if(!mp.isPlaying()){
-            changeIndex(0);
-        }
-    }
 }
